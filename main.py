39fe5ce18afe72d2e137be58c6d7e3971c48a4dc
vinicius almeida <87548413+pereirafunk448@users.noreply.github.com>
2022-03-11 00:29:35 +0000

    coloquei mais uma rota

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ app = Flask('app')
 def hello_world():
   return '<h1>Minha primeira rota!</h1>'
 
+@app.route('/unifran')
+def unifran():
+  return '<h2>Universidade de Franca</h2>'
+
 app.run(host='0.0.0.0', port=8080)
