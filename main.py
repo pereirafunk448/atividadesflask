561aa0e20f5d596e527517ca3c5ea2a09a145f92
vinicius almeida <87548413+pereirafunk448@users.noreply.github.com>
2022-03-18 00:42:17 +0000

    espaco

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def unifran():
   return '<h2>Universidade de Franca</h2>'
 @app.route('/dashboard/<nome>')
 def dashboard(nome):
-  return f'Olá,{nome}'
+  return f'Olá, {nome}'
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
